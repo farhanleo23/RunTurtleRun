feb32bc5aa09547e0e98578cd68a965d7fe96f6a
Smitha Kolan <smitkolan@gmail.com>
2018-05-28 18:35:35 +0200

    smitha

---
+++ RunTurtleRun/Assets/Scripts/backgroundScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundScript : MonoBehaviour
+{
+    private Vector3 backPos; 
+
+    [SerializeField]
+    public float width = 0f; 
+    [SerializeField]
+    public float height = 0f; 
+    private float X; 
+    private float Y;
+    void OnBecameInvisible()
+    {
+        //calculate current position
+        backPos = gameObject.transform.position;
+        //calculate new position
+        print(backPos);
+        X = backPos.x + width * 2;
+        Y = backPos.y + height * 2;
+        //move to new position when invisible
+        gameObject.transform.position = new Vector3(X, Y, 0f);
+    }
+
+}
