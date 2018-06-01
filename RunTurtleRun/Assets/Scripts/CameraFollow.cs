168c48c83d3a42b3e84752a8151064544acaf112
farhanleo23 <farhanleo23@gmail.com>
2018-06-01 19:05:16 +0200

    Name changes of file
    Changed turtlemovment to camera follow and turtleleftRight to TurtleMovement

---
+++ RunTurtleRun/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTowardsTarget();
+
+      
+    }
+
+
+    private void MoveTowardsTarget()
+    {
+        //the speed, in units per second, we want to move towards the target
+        float speed = 1;
+        //move towards the center of the world (or where ever you like)
+        Vector3 targetPosition = new Vector3(0, 0, 0);
+
+        Vector3 currentPosition = this.transform.position;
+        //first, check to see if we're close enough to the target
+        if (Vector3.Distance(currentPosition, targetPosition) > .1f)
+        {
+            Vector3 directionOfTravel = currentPosition - targetPosition;
+            //now normalize the direction, since we only want the direction information
+            directionOfTravel.Normalize();
+            //scale the movement on each axis by the directionOfTravel vector components
+
+            this.transform.Translate(
+                (directionOfTravel.x * speed * Time.deltaTime),
+                (directionOfTravel.y * speed * Time.deltaTime),
+                (directionOfTravel.z * speed * Time.deltaTime),
+                Space.World);
+        }
+    }
+
+
+}
+
+
