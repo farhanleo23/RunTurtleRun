d82dff186d45e9c88902dd98771993befdd90608
Smitha Kolan <smitkolan@gmail.com>
2018-06-01 21:24:00 +0200

    movement of bg

---
+++ RunTurtleRun/Assets/Scripts/scrollscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollscript : MonoBehaviour
+{
+
+    float scrollspeed = -5f;
+    Vector3 startPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollspeed, 20);
+        //transform.position = Vector3.MoveTowards(startPos, s,  Time.deltaTime);
+        transform.position = startPos + Vector3.forward * newPos;
+
+        GameObject go = GameObject.Find("temp");
+        /*if (go != null)
+        {
+            newPos = go.transform
+        }
+        else
+        {
+            Debug.Log("House not found");*/
+    }
+
+}
